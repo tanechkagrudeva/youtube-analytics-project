301c51c36f19ef3b95558a6dce255049eecee166
Татьяна <tanechka201286grudeva@gmail.com>
2023-04-11 13:20:55 +0300

    Added json

--- src/channel.py
+++ src/channel.py
@@ -1,43 +1,52 @@
 import json
 import os
-
+from dotenv import load_dotenv
 from googleapiclient.discovery import build
 
 
 """YT_API_KEY из гугла и он вставлен в переменные окружения"""
-
-os.environ['YT_API_KEY'] = 'UCMCgOm8GZkHp8zJ6l7_hIuA'
-api_key: str = os.getenv('YT_API_KEY')
-
-"""Cпециальный объект для работы с API"""
-youtube = build('youtube', 'v3', developerKey=api_key)
-
+load_dotenv()
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('YT_API_KEY')
+
+    """Cпециальный объект для работы с API"""
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.channel_info = Channel.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = self.channel_info['items'][0]['snippet']['title']
+        self.description = self.channel_info['items'][0]['snippet']['description']
+        self.url = self.channel_info['items'][0]['snippet']['thumbnails']['default']['url']
+        self.subscriberCount = self.channel_info['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel_info['items'][0]['statistics']['videoCount']
+        self.viewCount = self.channel_info['items'][0]['statistics']['viewCount']
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
 
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        print(json.dumps(self.channel_info, indent=2, ensure_ascii=False))
 
-    def get_service(self):
+    @classmethod
+    def get_service(cls):
         """возвращает объект для работы с YouTube API"""
-        id_channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        title_channel = youtube.channels().list(snippet=self.channel_id, part='title').execute()
-        description_channel = youtube.channels().list(snippet=self.channel_id, part='description').execute()
-        url = youtube.channels().list(snippet=self.channel_id, part='thumbnails, default, url').execute()
-        subscriberCount = youtube.channels().list(statistics=self.channel_id, part='subscriberCount').execute()
-        video_count = youtube.channels().list(statistics=self.channel_id, part='videoCount').execute()
-        viewCount = youtube.channels().list(statistics=self.channel_id, part='viewCount').execute()
-        return id_channel, title_channel, description_channel, url, subscriberCount, video_count, viewCount
+        return cls.youtube
 
-    def to_json(self):
+
+    def to_json(self, falename):
         """сохраняющий в файл значения атрибутов экземпляра `Channel`"""
-        with open('vdud.json', 'w') as outfile:
-            json.dump(Channel.get_service(), outfile)
+
+        with open(falename, "a+") as fp:
+            dict_={
+                'title': self.title,
+                'description': self.description,
+                'url': self.url,
+                'subscriberCount': self.subscriberCount,
+                'video_count': self.video_count,
+                'viewCount': self.viewCount,
+                }
+            json.dump(dict_, fp, ensure_ascii=False)
