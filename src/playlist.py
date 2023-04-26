91ef1e9f8bed5f05fe8486e3797ab67aecccab2f
Татьяна <tanechka201286grudeva@gmail.com>
2023-04-26 21:03:37 +0300

    correct method

--- src/playlist.py
+++ src/playlist.py
@@ -21,9 +21,28 @@ class PlayList:
                                                        ).execute()
         self.title: str = self.playlist['items'][0]['snippet']['title']
         self.url = "https://www.youtube.com/playlist?list=" + playlist_id
-        self.video_response = PlayList.youtube.videos().list(part='contentDetails,statistics',
+        self.playlist_response = PlayList.youtube.videos().list(part='contentDetails,statistics',
                                                    id=','.join(self.playlist_id)
                                                    ).execute()
+        self.playlist_videos = self.youtube.playlistItems().list(playlistId=playlist_id,
+                                                                       part='contentDetails',
+                                                                       maxResults=50,
+                                                                       ).execute()
+        """ получить все id видеороликов из плейлиста"""
+        self.playlist_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
+
+        """получить информацио о видеороликах из плейлиста по их id"""
+        self.playlist_response = PlayList.get_service().videos().list(part='contentDetails,statistics',
+                                                             id=','.join(self.playlist_ids)
+                                                             ).execute()
+
+    @classmethod
+    def get_service(cls):
+        """
+        Возвращает объект для работы с YouTube API
+        """
+        youtube = build('youtube', 'v3', developerKey=cls.api_key)
+        return youtube
 
     @property
     def total_duration(self):
@@ -32,12 +51,14 @@ class PlayList:
 
         total_duration = datetime.timedelta(seconds=0)
 
-        for video in self.video_response['items']:
+        for video in self.playlist_response['items']:
             iso_8601_duration = video['contentDetails']['duration']
             duration = isodate.parse_duration(iso_8601_duration)
             total_duration += duration
 
         return total_duration
+
+
 
     def __str__(self):
         return f'"{self.total_duration}"'
@@ -46,7 +67,7 @@ class PlayList:
         """возвращает ссылку на самое популярное видео из плейлиста (по количеству лайков)"""
         popular_like = 0
         url = None
-        for video in self.video_response['items']:
+        for video in self.playlist_response['items']:
             like_count = int(video['statistics']['likeCount'])
             if popular_like < like_count:
                 popular_like = like_count
